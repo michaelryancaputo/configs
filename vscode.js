2a32afe8134cc972ec5784b575704852b866b3b8
Michael Caputo <mike@capu.to>
2017-01-24 10:03:50 -0800

    Add VS code config

---
+++ vscode.js
@@ -0,0 +1,7 @@
+{
+    "window.zoomLevel": 1,
+    "terminal.integrated.fontFamily": "'SourceCodePro+Powerline+Awesome Regular', Menlo, 'DejaVu Sans Mono', 'Lucida Console', monospace",
+    "vsicons.projectDetection.autoReload": true,
+    "typescript.check.tscVersion": false,
+    "prettier.formatOnSave": true
+}
