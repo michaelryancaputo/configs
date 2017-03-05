088becf0eba8214f6ab3f90c80314fe240a2d6ed
Michael Caputo <mike@capu.to>
2017-03-04 16:55:45 -0800

    Update vscode.js

--- vscode.js
+++ vscode.js
@@ -1,10 +1,19 @@
 {
-    "window.zoomLevel": 1,
+    "window.zoomLevel": 0,
+    "editor.fontFamily": "'Monoid', Fira Code",
+    "editor.fontLigatures": true,
+    "editor.fontSize": 12,
+    "editor.lineHeight": 22,
     "terminal.integrated.fontFamily": "'SourceCodePro+Powerline+Awesome Regular', Menlo, 'DejaVu Sans Mono', 'Lucida Console', monospace",
     "vsicons.projectDetection.autoReload": true,
     "typescript.check.tscVersion": false,
-    "prettier.formatOnSave": true,
+    "editor.formatOnSave": true,
+    "editor.formatOnType": false,
+    "editor.formatOnPaste": false,
     "prettier.singleQuote": true,
     "prettier.trailingComma": true,
-    "prettier.bracketSpacing": true
+    "prettier.bracketSpacing": true,
+    "prettier.printWidth": 120,
+    "workbench.sideBar.location": "right",
+    "workbench.iconTheme": "vs-minimal"
 }
